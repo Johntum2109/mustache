aa02f1b00a3c860c076f797902ca429a443da749
Johntum2109 <78987759+Johntum2109@users.noreply.github.com>
2021-11-11 18:57:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,59 @@
-function preload() {}
+var noseX = 0;
+var noseY = 0;
 
+// Preload
+function preload() 
+{
+    clownNose = loadImage("https://i.postimg.cc/PrHGfLP7/mustash-2-217178.png");
+}
+
+// Setup
 function setup() 
-{
+{ 
     // Canvas
-    canvas = createCanvas(550, 550);
-    canvas.position(660, 200); 
+    canvas = createCanvas(600, 480);
+    canvas.center();
+
+    // Webcam
+    webcam = createCapture(VIDEO);
+    webcam.size(600, 480);
+    webcam.hide();
+
+    poseNet = ml5.poseNet(webcam, modelloaded);
+    poseNet.on('pose', gotPose);
 }
 
+// Draw
 function draw() 
-{
+{ 
+    image(webcam,0, 0, 600, 480);
 
+    image(clownNose, noseX, noseY, 120, 120);
 }
+
+// Apply the Filtieuer and save the Image
+function apply() 
+{ 
+    save("mySelfiePictureWithFilterTakenByJohnKhokhar.jpg");
+}
+
+function modelloaded() 
+{  
+    console.log("Model Loaded...");
+}
+
+function gotPose(result) 
+{ 
+    if (result.length > 0) 
+    { 
+        console.log(result);
+        console.log("Nose X" + result[0].pose.nose.x);
+        console.log("Nose Y" + result[0].pose.nose.y);
+
+        // Nose Positions
+        noseX = result[0].pose.nose.x - 55;
+        noseY = result[0].pose.nose.y - 38;
+    }
+}
+
+// noice
